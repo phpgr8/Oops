f5d0efe2c42a193a61f85bed31f95a12b7ad7960
PHP <42833018+phpgr8@users.noreply.github.com>
2019-12-16 15:29:24 +0530

    Create construct.php

---
+++ construct.php
@@ -0,0 +1,17 @@
+<?php
+class Abc
+{
+    // function __construct() // Construct hamesa pahle call hota hai
+    // {
+    //     echo "Hello World !";
+    // }
+    function Abc()
+    {
+        echo "User Define Construct";// This Is User Define Construct
+    }
+}
+$obj=new Abc;
+
+
+
+?>
